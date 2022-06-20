619c2929c1bf1d2fa4d9fe56bf70ea45abbd4235
caiocampoos <caiocampoosbr@gmail.com>
2022-06-19 22:27:31 -0400

    add: hub

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ var addr = flag.String("addr", "8080", "http service address")
 func servHome(w http.ResponseWriter, r *http.Request) {
         log.Println(r.URL)
         if r.URL.Path != "/" {
-                http.Error(w, "Not Found", htt.StatusNotFound)
+                http.Error(w, "Not Found", http.StatusNotFould)
                 return
         }
         if r.Method != http.MethodGet {
@@ -21,3 +21,17 @@ func servHome(w http.ResponseWriter, r *http.Request) {
         http.ServeFile(w, r, "home.html")
 }
 
+
+func main() {
+        flag.Parse()
+        hub := newHub()
+        go hub.run()
+        http.HandleFunc("/", servHome)
+        http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+                serveWs(hub, w, r)
+        })
+        err := http.ListenAndServe(*addr, nil)
+        if err != nil {
+                log.Fatal("ListenAndServe: ", err)
+        }
+}
